465ecf56cf640959b804c652649796c88d2d058a
matheus <matheus200713@gmail.com>
2018-09-03 19:44:38 -0400

    layout, botão excluir

--- Front-end/casa-facil/src/views/DetalheImovel.vue
+++ Front-end/casa-facil/src/views/DetalheImovel.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div>
+    Hello World!
+  </div>
 </template>
 
 <script>
--- Front-end/casa-facil/src/views/ListagemAnuncios.vue
+++ Front-end/casa-facil/src/views/ListagemAnuncios.vue
@@ -2,15 +2,18 @@
   <div style="padding: 15px; margin-top: 45px;">
     <loader-modal :show-modal="showModal"></loader-modal>
     <b-card title="Anuncios cadastrados">
-      <b-card v-for="anuncio in anuncios" style="margin: 15px; padding-left: 0px; cursor: pointer">
+      <b-card v-for="anuncio in anuncios" style="margin: 15px; padding-left: 0px; cursor: pointer; position: relative">
         <div class="row">
           <div class="col-sm-12 col-md-4 col-lg-5">
             <b-img alt="Thumbnail" :src="anuncio.imagensAnuncios[0].imagemUrl" style="width: 300px; height: 250px;"/>
           </div>
-          <div class="col-sm-12 col-md-4 col-lg-7">
+          <div class="col-sm-12 col-md-4 col-lg-6">
             <h3>{{anuncio.titulo}}</h3>
-            <p class="col-sm-12 col-md-4 col-lg-12" style="width: 100%">{{anuncio.descricao}}</p>
-            <p class="text-right text-bottom" style="font-weight: bold">Valor: {{anuncio.valor}}</p>
+            <p class="col-sm-12 col-md-4 col-lg-12" id="descricao">{{anuncio.descricao}}</p>
+            <p class="text-right text-bottom" id="valor">Valor: {{anuncio.valor}}</p>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-1">
+            <b-button variant="danger" id="excluir">Excluir</b-button>
           </div>
         </div>
       </b-card>
@@ -54,5 +57,25 @@ export default {
 </script>
 
 <style scoped>
+  #valor {
+    font-weight: bold;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin-right: 10px;
+  }
 
+  #descricao {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    max-height: 120px;
+    width: 100%;
+  }
+
+  #excluir {
+    position: absolute;
+    top: 50%;
+    margin-top: -25px;
+    right: 0;
+  }
 </style>
