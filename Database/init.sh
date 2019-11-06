14f7d95681189e23f4c4c383d8e6a17cba7677c6
Wender <wendergalan2014@hotmail.com>
2019-11-05 22:35:17 -0400

    Atualizando o docker-compose

---
+++ Database/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+echo 'Começando a execução da inicialização do banco de dados'
+## Popula o banco de dados
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/imagens.sql
+#psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /docker-entrypoint-initdb.d/enderecos.sql
+#psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /docker-entrypoint-initdb.d/anuncios.sql
+
+#echo 'Começando a execução da inicialização do banco de dados'
+## Cria o banco de dados
+#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /init.sql
+## Popula o banco de dados
+#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /populate/imagens.sql
+#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /populate/enderecos.sql
+#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /populate/anuncios.sql
