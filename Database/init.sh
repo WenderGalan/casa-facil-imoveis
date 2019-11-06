e8960c12bb9606131cc51542015f0f6ff2c14b99
Wender <wendergalan2014@hotmail.com>
2019-11-06 12:35:57 -0400

    Update init.sh

--- Database/init.sh
+++ Database/init.sh
@@ -6,13 +6,3 @@ psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" 
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/enderecos.sql
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/anuncios.sql
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/imagens.sql
-
-#psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /docker-entrypoint-initdb.d/anuncios.sql
-
-#echo 'Começando a execução da inicialização do banco de dados'
-## Cria o banco de dados
-#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /init.sql
-## Popula o banco de dados
-#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /populate/imagens.sql
-#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /populate/enderecos.sql
-#sqlplus "${POSTGRES_USER}"/"${POSTGRES_PASSWORD}" /populate/anuncios.sql
