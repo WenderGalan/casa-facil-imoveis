8d170d86f96cb5ebe11c048f9fe8b193ca696604
Wender <wendergalan2014@hotmail.com>
2019-11-07 22:38:17 -0400

    Update docker-compose

--- Front-end/casa-facil/src/util/constantes.js
+++ Front-end/casa-facil/src/util/constantes.js
@@ -1,6 +1,6 @@
 export default {
   // API_URL: 'https://casa-facil-imoveis.herokuapp.com',
-  API_URL: 'http://localhost:9095',
+  API_URL: 'http://localhost:8080',
   // API_URL: 'http://192.168.1.80:9095',
   // API_URL: 'http://192.168.1.155:9095',
   // API_URL: 'http://172.20.10.8:9095', //wifi wender
